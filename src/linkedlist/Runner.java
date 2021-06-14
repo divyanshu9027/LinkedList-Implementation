a26ca5cef9563b41549e0fffcb677cb33180bb22
divyanshu9027 <divyanshu.sharma_iot18@gla.ac.in>
2021-06-14 23:06:33 +0530

    LinkedList implementation is ready to run now.

--- src/linkedlist/Runner.java
+++ src/linkedlist/Runner.java
@@ -3,5 +3,19 @@ package linkedlist;
 public class Runner {
     public static void main(String[] args) {
 
+        LinkedList ll = new LinkedList();
+        ll.insert(5);
+        ll.insert(2);
+        ll.insert(5);
+        ll.insert(10);
+        ll.insert(1);
+
+        ll.inserAtstart(27);
+
+        ll.inserAt(2,79);
+
+        ll.deleteAt(2);
+        ll.show();
+
     }
 }
